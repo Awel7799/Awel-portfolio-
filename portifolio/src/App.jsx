e546fddcbedea306a153abcba037578cea3ce5d8
Awel7799 <awelkassim45@gmail.com>
2025-06-26 11:35:35 -0700

    fix folder error

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./component/navbar/Navbar";
 import HeroSection from "./component/heroSection/HeroSection";
-import About from "./component/About/About";
+import About from "./component/about/About";
 import Skill from "./component/skillsetSection/skill";
 import Project from "./component/project/Project";
 import Experiance from "./component/Experiance/Experiance";
